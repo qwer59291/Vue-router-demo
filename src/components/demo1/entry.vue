e0fc3b6d3c6dd4c2a3f2b0c18837aeab95d76849
卢振宇 <406622649@qq.com>
2019-10-28 14:12:50 +0800

    'demo1路径修改'

--- src/components/demo1/entry.vue
+++ src/components/demo1/entry.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <router-link to="/user/15">User 15</router-link>
+    <!-- 命名路由 -->
+    <router-link :to="{ name: 'user', params: { id: 123 }}">User 15</router-link> 
+    <!-- 命名路由 -->
     <router-link to="/user/16">User 16</router-link>
     <router-link to="/user/120">User 120</router-link>
     <router-view></router-view>
   </div>
 </template>
 <script>
-import router from "../../router/demo1/index.js";
+import router from "router/demo1/index.js";
 export default {
   data() {
     return {};
