41c6e9128f87bef1f49e3e4cd034ed69ba3e09bd
卢振宇 <406622649@qq.com>
2019-10-28 14:11:54 +0800

    '命令路由Demo'

---
+++ src/components/demo2/1/children/foo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>this is foo</div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ src/components/demo2/1/entry.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- 命名视图 -->
+  <div>
+    <h1>Named Views</h1>
+    <ul>
+      <li>
+        <router-link to="/">/</router-link>
+      </li>
+      <li>
+        <router-link to="/other">/other</router-link>
+      </li>
+    </ul>
+    <router-view class="view one"></router-view>
+    <router-view class="view two" name="a"></router-view>
+    <router-view class="view three" name="b"></router-view>
+  </div>
+</template>
+<script>
+import router from "router/demo2/index.js";
+export default {
+  data() {
+    return {};
+  },
+  router
+};
+</script>
+<style scoped>
+</style>
